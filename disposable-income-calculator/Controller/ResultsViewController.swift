3907aff60df699e4d45b11e1406e79a6a7561e5d
Alberto <aldo10012@gmail.com>
2021-01-11 13:41:08 -0800

    added segment controler to switch between anuually and monthly

--- disposable-income-calculator/Controller/ResultsViewController.swift
+++ disposable-income-calculator/Controller/ResultsViewController.swift
@@ -13,12 +13,15 @@ class ResultsViewController: UIViewController {
     var federalTax: Int = 0
     var ficaTax: Int = 0
     var stateTax: Int = 0
+    var payPeriod: String = "year"
     
     @IBOutlet weak var netIncomeLabel: UILabel!
     @IBOutlet weak var grossIncomeLabel: UILabel!
     @IBOutlet weak var federalTaxLabel: UILabel!
     @IBOutlet weak var ficaTaxLabel: UILabel!
     @IBOutlet weak var stateTaxLabel: UILabel!
+    @IBOutlet weak var payPeriodSegmentControler: UISegmentedControl!
+    @IBOutlet weak var payPeriodLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,15 +33,45 @@ class ResultsViewController: UIViewController {
         print("fica tax \(ficaTax) ")
         print("state tax \(stateTax) ")
         
+        updateUI()
+    }
+    
+    @IBAction func payPeriodSegmentChanged(_ sender: UISegmentedControl) {
+        switch payPeriodSegmentControler.selectedSegmentIndex{
+        case 0:
+            print("annual is selected")
+            grossIncome *= 12
+            federalTax *= 12
+            ficaTax *= 12
+            stateTax *= 12
+            payPeriod = "year"
+            updateUI()
+            
+        case 1:
+            print("monthly is selected")
+            grossIncome /= 12
+            federalTax /= 12
+            ficaTax /= 12
+            stateTax /= 12
+            payPeriod = "month"
+            updateUI()
+        default:
+            print("error")
+        }
+        
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil) 
+    }
+    
+    func updateUI(){
+        payPeriodLabel.text = "per \(payPeriod)"
         netIncomeLabel.text = "$\(grossIncome - federalTax - ficaTax - stateTax)"
         grossIncomeLabel.text = "$\(grossIncome)"
         federalTaxLabel.text = "- $\(federalTax)"
         ficaTaxLabel.text = "- $\(ficaTax)"
         stateTaxLabel.text = "- $\(stateTax)"
-    }
-    
-    @IBAction func recalculatePressed(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil) 
     }
     
     /*
